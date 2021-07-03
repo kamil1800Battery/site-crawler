3d3f7cca6e79f1a7cb70e340c67dfafafb7d7b15
kamil1800Battery <kamil.aqil@1800battery.com>
2021-07-02 22:28:44 -0700

    bot is janky

--- crawlerFunctions.js
+++ crawlerFunctions.js
@@ -7,53 +7,85 @@ const puppeteer = require('puppeteer');
 
 exports.addSuperChargerToCart = async () => {
     const browser = await puppeteer.launch({
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         headless: false,
-        devtools : true,
+        devtools: true,
         args: ['--ignore-certificate-errors']
     });
     const page = await browser.newPage();
-    await page.goto('https://localhost:443');
-    await page.setGeolocation({latitude: 59.95, longitude: 30.31667});
-    // wait for modal overlay
-    await page.waitForSelector(`body > div:nth-child(12) > div > div > div > div > div.place-selection.extended > div > span > input`).catch((err)=>{
-        // there is no location modal 
-        
+
+    page.setViewport({
+        width: 768,
+        height: 1024,
+        deviceScaleFactor: 1,
     })
-    // click modal overlay
-  
 
-    // enter dana point for location
-    
-    await page.type(`body > div:nth-child(12) > div > div > div > div > div.place-selection.extended > div > span > input`,'Dana Point',{delay: 100})
+    await page.setGeolocation({ latitude: 33.795878099999996, longitude: -117.9134853 });
+    await page.goto('https://localhost:443');
 
-    // await page.waitForTimeout(3000)
+    // wait to click use my location
+    await page.on('load')
+    console.log(`PAGE LOADED`);
+    // wait for background
+    await page.waitForSelector('#__next > div.theme-default > div:nth-child(2) > div.home-categories-wrapper > div')
+        .then(async () => {
+            /*
+                 clicked background to get rid of know your location
+            */
+            await page.click(`#__next > div.theme-default > div:nth-child(2) > div.home-categories-wrapper > div`).then(() => {
+                console.log(`clicked button`);
+            })
+        })
 
-    await page.keyboard.press('Enter')
+    // input city name
+    await page.type('#place-selection > div > div > div > span > input', "Anaheim", { delay: 100 })
 
-    await page.waitForTimeout(5000)
+    // press enter
+    await page.keyboard.down('Enter');
 
-    await page.type('#category-selection > div > div > input', 'edlebrock',{delay: 100})
+    // search for audi a5
+    await page.type('#category-selection > div > div > input', 'AUDI A5')
 
-    await page.waitForSelector(`#react-autowhatever-1-section-2-item-0`)
-    await page.click(`#react-autowhatever-1-section-2-item-0`)
+    // press enter
+    await page.keyboard.down('Enter');
 
-    // select instalation
-    await page.waitForSelector('#__next > div.theme-default > div.page.product > div.inner > div > div > div.col-xl-9 > div:nth-child(1) > div.col-lg-5.col-md-6.col-12.right-sidebar > div:nth-child(2) > div > div.add-to-cart-selector > div.product-options-container > div:nth-child(2) > button')
-    await page.click('#__next > div.theme-default > div.page.product > div.inner > div > div > div.col-xl-9 > div:nth-child(1) > div.col-lg-5.col-md-6.col-12.right-sidebar > div:nth-child(2) > div > div.add-to-cart-selector > div.product-options-container > div:nth-child(2) > button')
 
-    // click buy now
-    await page.click('#__next > div.theme-default > div.page.product > div.inner > div > div > div.col-xl-9 > div:nth-child(1) > div.col-lg-5.col-md-6.col-12.right-sidebar > div:nth-child(2) > div > div.add-to-cart-selector > div.add-to-cart-container > div > button.btn.cart')
+    // click model 
+    await page.waitForSelector('#__next > div.theme-default > div.page.search > div > div > div.ais-Hits > ul > li:nth-child(8) > div > a').then(async () => {
+        await page.click('#__next > div.theme-default > div.page.search > div > div > div.ais-Hits > ul > li:nth-child(8) > div > a')
+    }).catch((err) => {
+        console.log(`something went wrong clicking car model`);
+    })
 
-    // click return to cart
-    await page.waitForSelector(`#__next > div.theme-default > div:nth-child(2) > div > div > main > div > div > div.cart-content.hide-on-mobile > div > div.cart_orderSummaryContainer__3udGU > div.links > a.btn.return.mb-3`)
-    await page.click(`#__next > div.theme-default > div:nth-child(2) > div > div > main > div > div > div.cart-content.hide-on-mobile > div > div.cart_orderSummaryContainer__3udGU > div.links > a.btn.return.mb-3`)
 
-    // go to home page
-    await page.waitForSelector(`#header-new > div > div > div.col.logo-col > div > div.with-menu-icon > a`)
-    await page.click('#header-new > div > div > div.col.logo-col > div > div.with-menu-icon > a')
-    browser.close()
+    // click engine model 
+    await page.waitForSelector('#__next > div.theme-default > div.page.collection > div > div > div > div > div.visible > div > div > div > a').then(async () => {
+        await page.click('#__next > div.theme-default > div.page.collection > div > div > div > div > div.visible > div > div > div > a')
+    }).catch((err) => {
+        console.log(`something went wrong getting engine model`);
+    })
+
+
+    // schedule install 
+    await page.waitForSelector('#Mobile\ Delivery\ or\ Service\  > div.inner > div > div:nth-child(2) > div > div.available-services > div > div.add-to-cart-container > div.primary > button').then(async () => {
+        await page.click('#Mobile\ Delivery\ or\ Service\  > div.inner > div > div:nth-child(2) > div > div.available-services > div > div.add-to-cart-container > div.primary > button')
+    })
+
+    // enter personal info
+    await page.type('#guestFirstName', 'Kamil')
+
+    await page.type('#guestLastName', "Aqil")
+
+    await page.type('#guestEmail', 'kaqil91@gmail.com')
+
+    await page.type('#undefinedPhone', '+17147878471')
+
+    // click next 
+    await page.waitForSelector('#next-button').then(async () => {
+        await page.click('#next-button')
+    }).catch((err) => {
+        console.log(`SOMETHING WENT WRONG TRYING TO CLICK NEXT AFTER PERSONAL INFO`);
+    })
+
 }
-
-
  
